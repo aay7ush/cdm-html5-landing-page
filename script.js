3090fc15206d3261a7c50c8efdf243495fd675ae
aay7ush <aay7ush@gmail.com>
2024-02-27 21:21:37 +0530

    add js logic

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 lucide.createIcons()
+
+const form = document.querySelector('form')
+const emailInput = document.querySelector('input[name="email"]')
+const errorText = document.querySelector('.error')
+
+// Regular expression pattern for email validation
+const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault()
+
+  const emailValue = emailInput.value
+
+  if (emailValue.match(emailPattern)) {
+    alert('Thank You for Subscribing!')
+    emailInput.value = ''
+  } else {
+    emailInput.style.borderColor = '#ff0000'
+    errorText.style.display = 'block'
+  }
+})
+
+emailInput.addEventListener('keydown', () => {
+  emailInput.style.borderColor = '#8a9093'
+  errorText.style.display = 'none'
+})
